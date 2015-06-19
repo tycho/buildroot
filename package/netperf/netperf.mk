c3a6da44efecfd031b0fd27386c46714719be722
Steven Noonan <steven@uplinklabs.net>
2015-06-19 19:41:39 +0000

    package/netperf: unbreak build with GCC 5.1
    The default C standard changed with the new GCC version. This causes the
    'inline' keyword semantics to change, which yields this build breakage:
    
        nettest_omni.o: In function `send_omni_inner':
        nettest_omni.c:(.text+0x5d63): undefined reference to `demo_interval_tick'
        nettest_omni.c:(.text+0x61fa): undefined reference to `demo_interval_tick'
        nettest_omni.c:(.text+0x69b7): undefined reference to `demo_interval_tick'
        nettest_omni.c:(.text+0x69c6): undefined reference to `demo_interval_tick'
        collect2: error: ld returned 1 exit status
    
    Adding an explicit -std=gnu89 to the CFLAGS unbreaks the build.
    
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- package/netperf/netperf.mk
+++ package/netperf/netperf.mk
@@ -6,7 +6,7 @@
 
 NETPERF_VERSION = 2.6.0
 NETPERF_SITE = ftp://ftp.netperf.org/netperf
-NETPERF_CONF_ENV = ac_cv_func_setpgrp_void=set
+NETPERF_CONF_ENV = ac_cv_func_setpgrp_void=set CFLAGS="$(TARGET_CFLAGS) -std=gnu89"
 NETPERF_CONF_OPTS = --enable-demo=yes
 NETPERF_LICENSE = netperf license
 NETPERF_LICENSE_FILES = COPYING
